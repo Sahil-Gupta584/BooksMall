f6f0a7a375592cfe96b406e159968d9bf491bbc1
sanchititvijay <sanchiitvijay@gmail.com>
2024-10-08 16:04:25 +0530

    added aboutus page

---
+++ app/components/AboutUs.jsx
@@ -0,0 +1,46 @@
+
+function AboutUs (){
+  return (
+    <div>
+        <div className='text-3xl text-[orange] font-semibold text-center my-8'>
+            About BooksMall
+        </div>
+        <div>
+            <p className='text-justify px-8 md:px-64 md:pt-4 pb-8'>BooksMall is a platform that connects book lovers across India. We believe that books should be accessible to everyone, and our platform is designed to make buying and selling books as easy as possible. Whether you are looking to pass on a beloved story or discover a new one, BooksMall is the place for you. Our platform empowers individuals to not only buy and sell books but also connect with sellers directly. Through our chat feature, you can negotiate the price, ensuring that both buyers and sellers get the best possible deal. We understand that a good bargain is part of the joy of finding a new read, and we are here to make that process as seamless and enjoyable as possible.</p>
+        </div>
+        <div className='px-8 md:px-24'>
+            <div className='flex md:flex-row gap-36 pb-12 md:pb-8 justify-evenly flex-col'>
+                <div className="md:w-[60%] md:">
+                    <p className='text-xl font-semibold my-4'>Seamless Book Trading</p>
+                    <p className='text-justify'>Welcome to our unique online book marketplace, where buying and selling books has never been more exciting and convenient. We believe that books should be accessible to everyone, whether you are looking to pass on a beloved story or discover a new one. Our platform empowers individuals to not only buy and sell books but also connect with sellers directly. Through our chat feature, you can negotiate the price, ensuring that both buyers and sellers get the best possible deal. We understand that a good bargain is part of the joy of finding a new read, and we are here to make that process as seamless and enjoyable as possible.</p>
+                </div>
+                <div className="pr-3 pt-3 max-md:mt-[-100px] bg-[orange] h-max w-max max-md:mx-auto rounded-md">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzyOqnl6Jpq15fUt8lCZu-ldQOEwpAtfnexA&s" alt="img1" className='w-[256px] h-[256px] border border-black'/>
+                </div>
+            </div>
+            <div className='flex md:flex-row-reverse gap-36 pb-12 md:pb-8 justify-evenly flex-col'>
+                <div className="md:w-[60%] md:">
+                    <p className='text-xl font-semibold my-4'>Community and Sustainability</p>
+                    <p className='text-justify'>Our mission is to create a community-driven marketplace that fosters a love for reading and sharing knowledge. We value sustainability and encourage the recycling of books, offering a platform where pre-loved books can find new homes. Whether you're a student looking for affordable textbooks, a collector searching for rare editions, or simply a reader wanting to explore different genres, our platform provides a space where everyone can benefit. With our easy-to-use interface, you can browse thousands of titles, communicate directly with sellers, and make informed purchasing decisions in real time.</p>
+                </div>
+                <div className="pr-3 pt-3 max-md:mt-[-100px] bg-[orange] h-max w-max max-md:mx-auto rounded-md">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwEmRxqvMCLYuCeuTSxy0f4a1JLF9QurPZNw&s" alt="img2" className='w-[256px] h-[256px] border border-black'/>
+                </div>
+            </div>
+            <div className='flex md:flex-row gap-36 pb-12 md:pb-8 justify-evenly flex-col'>
+                <div className="md:w-[60%] md:">
+                    <p className='text-xl font-semibold my-4'>Safe and Dynamic Marketplace</p>
+                    <p className='text-justify'>We pride ourselves on offering a safe, transparent, and dynamic environment for our users. Our chat-based bargaining feature sets us apart, allowing for personalized interactions between buyers and sellers. It adds a human touch to online transactions, making it more than just a business transaction. We are constantly improving our services, listening to feedback from our community, and working to ensure a positive experience for everyone. Join us today and be a part of a marketplace where books and stories come to life!</p>
+                </div>
+                <div className="pr-3 pt-3 max-md:mt-[-100px] bg-[orange] h-max w-max max-md:mx-auto rounded-md">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEHRhvpBOdCu5W7ilxMnJw6qpMzbmRCuRzNQ&s" alt="img1" className='w-[256px] h-[256px] border border-black'/>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+  )
+}
+
+export default AboutUs

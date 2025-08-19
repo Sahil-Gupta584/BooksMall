84739f8bc5b5f589466c67fe643fc7c81032ed9b
Sahil Gupta <guptas3067@gmail.com>
2025-08-19 06:40:10 +0000

    c

--- apps/backend/app/api/auth/[...all]/route.ts
+++ apps/backend/app/api/auth/[...all]/route.ts
@@ -1,21 +1,5 @@
-// export const { POST, GET } = toNextJsHandler(auth);
+import { auth } from "@/lib/auth";
+import { toNextJsHandler } from "better-auth/next-js";
 
-import { Books, connectDb } from "@repo/db";
-import { NextRequest, NextResponse } from "next/server";
+export const { POST, GET } = toNextJsHandler(auth);
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: Promise<{ bookId: string }> }
-) {
-  try {
-    const { bookId } = await params;
-    await connectDb();
-
-    const book = await Books.find({ _id: bookId }).populate("owner");
-    return NextResponse.json(book[0]);
-  } catch (error) {
-    return NextResponse.json({
-      error: error instanceof Error ? error.message : "Unknown error",
-    });
-  }
-}
